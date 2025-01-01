4555effb1bbde19502c33230a486a8aabfe40ed3
saif0110 <khanmohdtauheed4@gmail.com>
2025-01-01 10:52:38 +0530

    file changed

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 
-const filePath = process.argv[2];
+const filePath = path.join(__dirname, process.argv[2]);
+
 
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
